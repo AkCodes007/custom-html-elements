e69fb147e9d0e218f3b6886c8c23a13ef4a802ed
Akhil Robert <akhil@Akhils-MacBook-Pro.local>
2021-03-27 16:07:12 +0530

    Add custom toggle button

--- gulpfile.js
+++ gulpfile.js
@@ -5,18 +5,22 @@ const sass = require('gulp-sass');
 const sourcemaps = require('gulp-sourcemaps');
 const autoprefixer = require('gulp-autoprefixer');
 
+const SRC = './toggle/scss/*.scss'; // Where the scss files are located
+const DIST = './toggle/css'; // Where the output css should go
+const CHANGE = './toggle/scss/*.scss'; // which files should be watched for changes
+
 function _sass() {
   return gulp
-    .src('./slider/scss/*.scss')
+    .src(SRC)
     .pipe(sourcemaps.init())
     .pipe(sass())
     .pipe(autoprefixer())
     .pipe(sourcemaps.write('./maps'))
-    .pipe(gulp.dest('./slider/css'));
+    .pipe(gulp.dest(DIST));
 }
 
 function _watch() {
-  return gulp.watch('./slider/scss/', _sass);
+  return gulp.watch(CHANGE, _sass);
 }
 
 exports.default = _watch;
